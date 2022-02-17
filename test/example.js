a81360afa46692f0695dcf2db8e770b32c6ea0fa
fanlia <3093932086@qq.com>
2022-02-17 20:22:02 +0800

    first commit

---
+++ test/example.js
@@ -0,0 +1,51 @@
+
+const schema = `
+
+type Group {
+    userId: String
+    userName: String
+}
+
+type Groups {
+    count: Int
+    data: [Group]
+}
+
+type Query {
+    error: JSON
+    echo(message: JSON!): JSON
+    groups: Groups
+}
+`
+
+const api = {
+    error () {
+        throw new Error('error happend')
+    },
+    echo ({message}) {
+        return message
+    },
+}
+
+const resolver = {
+    Query: {
+        groups () {
+            return {count: 2, data: [{userId: 'userId'}]}
+        },
+    },
+    Group: {
+        userName (group) {
+            return `${group.userId}-userName`
+        }
+    },
+}
+
+const {
+    graphiql,
+} = require('..')
+
+const express = require('express')
+
+express()
+.use(graphiql({schema, api, resolver}))
+.listen(process.env.PORT || 3000)
